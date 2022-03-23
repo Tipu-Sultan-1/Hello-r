70a6e13af2aba701c72701cd9f4a8de2bd7e2942
Tipu Sultan <sultan.fateh.ali.tipu1@gmail.com>
2022-03-23 19:02:42 +0500

    Few lines added

--- testing.r
+++ testing.r
@@ -1,4 +1,10 @@
 # This is from Githhub
   print("Hello from Github")
   
+  name = "Sultan"
+  version = "Rstudio - In Writing"
   
+print(name)
+
+print("These lines has been written in RStudio")
+
